564877b318b0c4b5c505eec0ed5bf2fc71a3fc26
Mohammad Qady <112632549+MOQ2@users.noreply.github.com>
2025-08-03 12:35:53 +0300

    initial spring boot project

---
+++ src/test/java/com/example/e_learning_system/ELearningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_learning_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
