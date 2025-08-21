3d72a226a9e8e6a1f7d3ac36ebd97e72a992e74a
MOQ2 <mohammedoqady@gmail.com>
2025-08-21 11:26:07 +0300

    feat:interface : add AttachmentService interface for managing attachment operations

---
+++ src/main/java/com/example/e_learning_system/Interfaces/AttachmentService.java
@@ -0,0 +1,16 @@
+package com.example.e_learning_system.Interfaces;
+
+import com.example.e_learning_system.Dto.AttachmentDtos.AttachmentDto;
+import com.example.e_learning_system.Dto.AttachmentDtos.CreateAttachmentDto;
+import com.example.e_learning_system.Dto.AttachmentDtos.UpdateAttachmentDto;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+
+public interface AttachmentService {
+    void createAttachment(CreateAttachmentDto createAttachmentDto, int uploadedByUserId);
+    AttachmentDto getAttachmentById(Integer id);
+    void deleteAttachment(Integer id);
+    void updateAttachment(UpdateAttachmentDto updateAttachmentDto, int id, int uploadedByUserId);
+    ResponseEntity<Resource> downloadAttachment(Integer id);
+    byte[] getAttachmentFileData(Integer id);
+}
