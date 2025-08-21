9599537aac257bcabd5e37b5976ff4cb86f650ba
MOQ2 <mohammedoqady@gmail.com>
2025-08-21 11:26:31 +0300

    feat:interface : add CourseService interface for managing course operations

--- src/main/java/com/example/e_learning_system/Interfaces/CourseService.java
+++ src/main/java/com/example/e_learning_system/Interfaces/CourseService.java
@@ -2,7 +2,7 @@ package com.example.e_learning_system.Interfaces;
 
 import com.example.e_learning_system.Config.CourseStatus;
 import com.example.e_learning_system.Config.DifficultyLevel;
-import com.example.e_learning_system.Dto.CourseDto;
+import com.example.e_learning_system.Dto.CourseDtos.*;
 import com.example.e_learning_system.Dto.CreateCourseRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,18 +11,15 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public interface CourseService {
-    CourseDto createCourse(CreateCourseRequest request, Integer createdById);
-    CourseDto getCourseById(Integer id);
-    List<CourseDto> getCoursesByUser(Integer userId);
-    List<CourseDto> getActiveCourses(Pageable pageable);
-    List<CourseDto> getPublishedCourses(Pageable pageable);
-    List<CourseDto> getCoursesByStatus(CourseStatus status);
-    List<CourseDto> getCoursesByDifficulty(DifficultyLevel difficulty);
-    List<CourseDto> getFreeCourses();
-    List<CourseDto> getCoursesByPriceRange(BigDecimal minPrice, BigDecimal maxPrice);
-    CourseDto updateCourse(Integer id, CreateCourseRequest request);
-    CourseDto publishCourse(Integer id);
+
+    List<CourseSummaryDto>  getCourses();
+    Page<CourseSummaryDto> getCourses(CourseFilterDto filterDto, Pageable pageable);
+    CourseDetailsDto createCourse(CreateCourseDto request, Integer createdById );
+    CourseDetailsDto getCourseById(Integer id);
+    public void updateCourse(UpdateCourseDto updateCourseDto , int courseId);
     void deleteCourse(Integer id);
-    void deactivateCourse(Integer id);
-    List<CourseDto> searchCoursesByName(String name);
+    void deactivateCourse(Integer id , boolean deactivate);
+    void addMoudelToCourse (int courseId , int moudelId , int order);
+    void removeMoudelFromCourse (int courseId , int moudelId);
+
 }
