41693cb6e64513bdad8759c67ddec81a6d2edded
Your New Name <nourtomeh17@gmail.com>
2025-09-18 16:52:02 +0300

    add payment & user course access feature

--- Frontend/src/app/Services/Courses/course-service.ts
+++ Frontend/src/app/Services/Courses/course-service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
-import {map, Observable} from 'rxjs';
-import {Courses} from '../../courses/courses';
-
+import {map, Observable, switchMap} from 'rxjs';
+import {PaymentToDto , PaymentFromDto} from '../../models/payment_model';
+import {UserCourseAccessFromDto, UserCourseAccessToDto} from '../../models/userCourseAccessModel';
 export interface CourseInterface {
   id: number;
   name: string;
@@ -27,6 +27,38 @@ private api = `${environment.apiUrl}`
       .pipe(
         map(res => res.data)
       )
+  }
+
+  createPayment(payment : PaymentToDto): Observable<PaymentFromDto> {
+  return this.http.post<PaymentFromDto>(this.api+'/api/payments',payment);
+  }
+
+  grantAccess(grantDto: UserCourseAccessToDto): Observable<UserCourseAccessFromDto> {
+    const params = new URLSearchParams();
+    params.set('userId', grantDto.userId.toString());
+    params.set('courseId', grantDto.courseId.toString());
+    params.set('accessType', grantDto.accessType);
+    if (grantDto.paymentId) params.set('paymentId', grantDto.paymentId.toString());
+
+    return this.http.post<UserCourseAccessFromDto>(
+      `${this.api}/api/user-course-access/grant-course-access?${params.toString()}`,
+      null
+    );
+  }
+
+
+  payAndGrantAccess(payment: PaymentToDto, accessType: 'PURCHASED'): Observable<UserCourseAccessFromDto> {
+    return this.createPayment(payment).pipe(
+      switchMap(paymentRes => {
+        const grantDto: UserCourseAccessToDto = {
+          userId: payment.userId,
+          courseId: payment.courseId!,
+          accessType: accessType,
+          paymentId: paymentRes.id
+        };
+        return this.grantAccess(grantDto);
+      })
+    );
   }
 
 }
