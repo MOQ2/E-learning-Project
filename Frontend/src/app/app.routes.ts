8c4527d11f38862b9c23add74d483e58bdc6b2cb
Your New Name <nourtomeh17@gmail.com>
2025-09-22 11:10:18 +0300

    solve comments & add Auth guards to check user logged in before access to components

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,15 +1,14 @@
 import { Routes } from '@angular/router';
-import {Home} from './home/home';
 import {Login} from './auth/login/login';
 import {Signup} from './auth/signup/signup';
 import {Payment} from './payment/payment';
 import {Courses} from './courses/courses';
+import {authGuard} from './Services/Auth/auth-guard';
 
 export const routes: Routes = [
-  { path: '', component: Home},
   {path: 'login',  component:Login},
   {path: 'signUp',  component:Signup},
-  {path: 'payment',  component:Payment},
-  {path: 'courses', component:Courses}
+  {path: 'courses', component:Courses , canActivate:[authGuard]},
+  {path: 'payment',  component:Payment , canActivate:[authGuard]}
 ];
 
